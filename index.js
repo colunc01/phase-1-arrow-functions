dfcde97f8077c562555b31455f509fe929a08795
Cesar Colunga <cesara.colunga@gmail.com>
2023-01-12 17:30:09 -0600

    complete lab

---
+++ index.js
@@ -0,0 +1,10 @@
+// Divide function
+function divide (num1, num2){
+    return num1/num2;
+}
+
+// Arrow function for square
+const square = (num1) => num1 * num1;
+
+// Arrow function for add
+const add = (num1, num2) => num1 + num2;
